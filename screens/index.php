5912cbdb6b81074ad8b6f995346cc974e83d58c0
Button99 <christoskoubis@yahoo.com>
2021-04-23 15:22:26 +0300

    Style v2

---
+++ screens/index.php
@@ -0,0 +1,41 @@
+<?php
+    include("../imports/pageHeader.php");
+?>
+
+<section class="text">
+    Βρες το κατάλληλο ξενοδοχείο για σένα και την οικογένεια σου εύκολα και γρήγορα!
+</section>
+<section class="town-pictures">
+    <ul class="town-cards">
+        <li>
+            <a href="athens.php"><img src="../media/Athens.jpg" tag="Athens" />
+            <h3>Αθήνα</h3></a>
+            <p>Βρες όλα τα ξενοδοχεία που βρίσκονται στην περιοχή της Αθήνας μονο με ενα κλικ!</p>
+        </li>
+
+        <li>
+            <a href="thessaloniki.php"><img src="../media/Thessaloniki.jpg" tag="Thessaloniki"/>
+            <h3>Θεσσαλονίκη</h3></a>
+            <p>Βρες όλα τα ξενοδοχεία που βρίσκονται στην περιοχή της Θεσσαλονίκη μονο με ενα κλικ!</p>
+
+        </li>
+
+
+        <li>
+            <a href="patra.php"><img src="../media/Patra.jpg" tag="Patra"/>
+            <h3>Πάτρα</h3></a>
+            <p>Βρες όλα τα ξενοδοχεία που βρίσκονται στην περιοχή της Πάτρας μονο με ενα κλικ!</p>
+
+        </li>
+        <li>
+            <a href="larissa.php"><img src="../media/Larissa.jpg" tag="Larissa"/>
+            <h3>Λάρισα</h3></a>
+            <p>Βρες όλα τα ξενοδοχεία που βρίσκονται στην περιοχή της Λάρισας μονο με ενα κλικ!</p>
+
+        </li>
+    </ul>
+</section>
+<?php
+    include("../imports/pageFooter.php");
+
+?>
---
+++ screens/logIn.php
@@ -0,0 +1,18 @@
+<?php 
+    include("../imports/pageHeader.php");
+?>
+
+<section class="logIn-form">
+    <p>This is the log in page</p>
+    <form>
+        <label for="username"><br>
+        <input type="text" name="username" /><br>
+        <label for="password"><br>
+        <input type="password" name="password" /><br><br>
+        <button type="submit">Log in</button>
+    </form>
+</section>
+
+<?php
+    include("../imports/pageFooter.php");
+?>
