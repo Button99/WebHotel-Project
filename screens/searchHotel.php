6648daaa1024d23c955459b40fd7be45695291d2
Button99 <christoskoubis@yahoo.com>
2021-05-14 13:09:58 +0300

    Added Search

--- screens/searchHotel.php
+++ screens/searchHotel.php
@@ -5,10 +5,52 @@
 ?>
 <section>
     <?php
-        if($_SESSION["record"]) {
-            echo $_SESSION["record"];
+        if(isset($_GET["searchHotel"])) {
+            $res= formVal();
+            if($res == TRUE) {
+                echo "Error";
+                exit();
+            }
+            $recordsPerPage = 5;
+            if(isset($_GET["page"])) 
+                $curPage= $_GET["page"];
+            else 
+                $curPage= 1;
+    
+            $startIndex= ($curPage - 1) * $recordsPerPage = 5;
+
+            $district= $_SESSION["district"];
+            $rooms= $_SESSION["rooms"];
+            $date= $_SESSION["date"];
+
+            try {
+
+                $sql= "SELECT COUNT(hotelID) as recCount FROM `Hotels`;";
+                $stmt= $conn -> query($sql);
+                $record= $stmt-> fetch(PDO::FETCH_ASSOC);
+                $pages= ceil($record["recCount"]/ $recordsPerPage);
+                $stmt-> closeCursor();
+                $record= null;
+
+                $sql= "SELECT * FROM `Hotels` WHERE `district`=:district AND `numberOfRooms`>=:rooms  LIMIT :startIndex, :recordsPerPage; ";
+                $stmt= $conn->prepare($sql);
+    
+                $stmt-> bindParam("district", $district, PDO::PARAM_STR);
+                $stmt-> bindParam("rooms", $rooms, PDO::PARAM_STR);
+                $stmt-> bindParam("startIndex", $startIndex, PDO::PARAM_INT);
+                $stmt-> bindParam("recordsPerPage", $recordsPerPage, PDO::PARAM_INT);
+                $stmt-> execute();
+
+                while($record= $stmt-> fetch(PDO::FETCH_ASSOC)) {
+                    echo $record["hotelName"]; 
+                }
+
+            } catch(PDOException $e) {
+                echo "Error <br>" . $e -> getMessage();
+            }
+            $stmt-> closeCursor();
         }
-    ?>
+?>
 </section>
 
 <?php
--- serverSideVal/formSearchValidation.inc.php
+++ serverSideVal/formSearchValidation.inc.php
@@ -1,14 +1,10 @@
 <?php
-
-    include("../server/conn.inc.php");
-
-    $district= $_GET["dsc"];
-    $rooms= $_GET["room"];
-    $date= $_GET["date"];
-
-    $flag= FALSE;
-
-    if(isset($_GET["searchHotel"])) {
+    
+    function formVal() {
+        $district= $_GET["dsc"];
+        $rooms= $_GET["room"];
+        $date= $_GET["date"];
+        $flag= FALSE;
 
         if(!isset($district) && !isset($rooms) && !isset($date)) {
             echo "Συμπληρώστε όλα τα κενά!<br>";
@@ -19,13 +15,11 @@
         if(preg_match("^[a-zA-Z]+$", $district) || !isset($district)) {
             echo "Συμπληρώστε την περιοχή!<br>";
             $flag= TRUE;
-
         }
 
         if(($rooms < 0 || $rooms > 999) || !isset($rooms)) {
             echo "Συμπληρώστε τα άτομα!<br>";
             $flag= TRUE;
-
         }
 
         if(preg_match("/^\d{2}[./-]\d{2}[./-]\d{4}$/", $date) || !isset($date)) {
@@ -34,47 +28,12 @@
         }
 
         if($flag == TRUE) {
-            exit();
+            return $flag;
         }
 
-        $recordsPerPage = 5;
-        if(isset($_GET["page"])) 
-            $curPage= $_GET["page"];
-        else 
-            $curPage= 1;
-
-        $startIndex= ($curPage - 1) * $recordsPerPage = 5;
-
-        try {
-            $sql= "SELECT COUNT(hotelID) as recCount FROM `Hotels`;";
-            $stmt= $conn -> query($sql);
-            $record= $stmt-> fetch(PDO::FETCH_ASSOC);
-            $pages= ceil($record["recCount"]/ $recordsPerPage);
-            $stmt-> closeCursor();
-            $record= null;
-
-            $sql= "SELECT * FROM `Hotels` WHERE `district`=:district AND `numberOfRooms`>=:rooms  LIMIT :startIndex, :recordsPerPage; ";
-            $stmt= $conn->prepare($sql);
-
-            $stmt-> bindParam("district", $district, PDO::PARAM_STR);
-            $stmt-> bindParam("rooms", $rooms, PDO::PARAM_STR);
-            $stmt-> bindParam("startIndex", $startIndex, PDO::PARAM_INT);
-            $stmt-> bindParam("recordsPerPage", $recordsPerPage, PDO::PARAM_INT);
-            $stmt-> execute();
-
-            $count= $stmt-> rowCount();
-            $record= $stmt-> fetch(PDO::FETCH_OBJ);
-
-            if($record) {
-                header("Location: ../screens/searchHotel.php");
-            }
-
-            else {
-                echo "Error! cant find place<br>";
-            }
-        } catch(PDOException $e) {
-            echo "Error <br>" . $e -> getMessage();
-        }
-        $stmt-> closeCursor();
+        $_SESSION["district"]= $district;
+        $_SESSION["rooms"]= $rooms;
+        $_SESSION["date"]= $date;
+        return $flag;
     }
 ?>
